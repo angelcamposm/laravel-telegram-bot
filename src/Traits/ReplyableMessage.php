db279e630768ace68892064e060620ceb2349009
angelcamposm <angel.campos.m@outlook.com>
2021-10-16 17:21:12 +0200

    Updated ReplyableMessage.php, added method getSendingWithoutReplyParameterValue.

--- src/Traits/ReplyableMessage.php
+++ src/Traits/ReplyableMessage.php
@@ -8,9 +8,9 @@ trait ReplyableMessage
      * If the message is a reply, ID of the original message.
      *
      * @required no
-     * @var int
+     * @var string|int
      */
-    protected int $reply_to_message_id;
+    protected string|int $reply_to_message_id;
 
     /**
      * Pass True, if the message should be sent even if the specified replied-to message is not found.
@@ -30,4 +30,14 @@ trait ReplyableMessage
      * @var string
      */
     protected string $reply_markup;
+
+    /**
+     * Return the value or parameter allow_sending_without_reply
+     *
+     * @return bool
+     */
+    private function getSendingWithoutReplyParameterValue(): bool
+    {
+        return $this->allow_sending_without_reply ?? false;
+    }
 }
