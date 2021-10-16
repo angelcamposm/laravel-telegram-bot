7929cfd3c09e17faf3f0cd66808758c11c1408ce
angelcamposm <angel.campos.m@outlook.com>
2021-10-16 23:25:19 +0200

    Added RestMethod.php class.

---
+++ src/Enums/RestMethod.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of acamposm\laravel-telegram-bot package.
+ *
+ * Copyright (c) Angel Campos Muñoz <angel.campos.m@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acamposm\TelegramBot\Enums;
+
+class RestMethod
+{
+    public const GET = 'GET';
+    public const HEAD = 'HEAD';
+    public const OPTIONS = 'OPTIONS';
+    public const PATCH = 'PATCH';
+    public const POST = 'POST';
+    public const PUT = 'PUT';
+    public const DELETE = 'DELETE';
+}
