111158a8044f3415b0ba0a570a5e589d76d4e34a
angelcamposm <angel.campos.m@outlook.com>
2021-10-16 19:00:16 +0200

    Updated trait ParseableMessage.php

--- src/Traits/ParseableMessage.php
+++ src/Traits/ParseableMessage.php
@@ -2,6 +2,7 @@
 
 namespace Acamposm\TelegramBot\Traits;
 
+use Acamposm\TelegramBot\Contracts\RequestMethod;
 use Acamposm\TelegramBot\Enums\ParseStyle;
 use Acamposm\TelegramBot\Exceptions\ValueException;
 
@@ -17,27 +18,40 @@ trait ParseableMessage
     protected string $parse_mode;
 
     /**
+     * Mode for parsing entities in the message text.
+     *
+     * @param string $parse_mode
+     *
+     * @return \Acamposm\TelegramBot\Contracts\RequestMethod
+     * @throws \Acamposm\TelegramBot\Exceptions\ValueException
+     */
+    public function setParseMode(string $parse_mode): RequestMethod
+    {
+        self::verifyParseMode($parse_mode);
+
+        $this->parse_mode = $parse_mode;
+
+        return $this;
+    }
+
+    /**
      * Return the parse mode for the message.
      *
      * @return string
-     *
-     * @throws ValueException
      */
     private function getParseMode(): string
     {
-        if (isset($this->parse_mode)) {
-            $this->verifyParseMode();
-        }
-
         return $this->parse_mode;
     }
 
     /**
+     * Verifies if parse_mode is a valid mode of parsing messages.
+     *
      * @throws ValueException
      */
-    private function verifyParseMode(): void
+    private static function verifyParseMode(string $parse_mode): void
     {
-        if (! in_array($this->parse_mode, ParseStyle::getValidStyles())) {
+        if (! in_array($parse_mode, ParseStyle::getValidStyles())) {
             throw ValueException::UnknownParseStyle();
         }
     }
